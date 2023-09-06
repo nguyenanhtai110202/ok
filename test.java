0fac00250f868b7d88d601a15cfaed7bb89d9c71
Duyyyy <duy123.lqm@gmail.com>
2023-09-07 01:08:35 +0700

    ok

---
+++ test.java
@@ -0,0 +1,13 @@
+
+class App{
+int j = 2;
+switch (j ){ 
+    case 2:
+System.out.println("value is two");
+case 2 + 1:
+System.out.println("value is three");
+break;
+default:
+System.out.println("value is" + j);
+break;
+}
